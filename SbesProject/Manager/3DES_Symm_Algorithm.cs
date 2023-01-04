af3bd4bf4bb9a9acb23da4ee025fe8bd2e7e6ad9
Bojana Živanić <zivanic.pr122.2019@uns.ac.rs>
2023-01-04 13:41:50 +0100

    Encryption methods

---
+++ SbesProject/Manager/3DES_Symm_Algorithm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    public class _3DES_Symm_Algorithm
+    {
+        public static byte[] Encrypt(byte[] source, string key)
+        {
+            TripleDESCryptoServiceProvider desCryptoProvider = new TripleDESCryptoServiceProvider();
+            MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider();
+
+            byte[] byteHash;
+            byte[] byteBuff;
+
+            byteHash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(key));
+            desCryptoProvider.Key = byteHash;
+            desCryptoProvider.Mode = CipherMode.ECB;
+            byteBuff = source;
+
+            byte[] encoded =
+                desCryptoProvider.CreateEncryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length);
+            return encoded;
+        }
+
+        public static byte[] Decrypt(byte[] encodedText, string key)
+        {
+            TripleDESCryptoServiceProvider desCryptoProvider = new TripleDESCryptoServiceProvider();
+            MD5CryptoServiceProvider hashMD5Provider = new MD5CryptoServiceProvider();
+
+            byte[] byteHash;
+            byte[] byteBuff;
+
+            byteHash = hashMD5Provider.ComputeHash(Encoding.UTF8.GetBytes(key));
+            desCryptoProvider.Key = byteHash;
+            desCryptoProvider.Mode = CipherMode.ECB;
+            byteBuff = encodedText;
+
+            byte[] plaintext = desCryptoProvider.CreateDecryptor().TransformFinalBlock(byteBuff, 0, byteBuff.Length);
+            return plaintext;
+        }
+    }
+}
